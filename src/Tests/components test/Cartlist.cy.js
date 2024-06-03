709bb93f49a62d74216d8169011515131d697da8
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2024-06-03 09:33:35 +0000

    Testing CartList component with cypress

---
+++ src/Tests/components test/Cartlist.cy.js
@@ -0,0 +1,29 @@
+describe("CartList.vue", () => {
+  beforeEach(() => {
+    cy.visit("./components/Cartlist.vue"); 
+  });
+
+  it("renders the correct number of products", () => {
+    cy.get(".product-container").should("have.length", 5); 
+  });
+
+  it("displays product details correctly", () => {
+    cy.get(".product-container").each(($product, index) => {
+      cy.wrap($product)
+        .find("h3")
+        .should("contain.text", `Product ${index}`); 
+      cy.wrap($product)
+        .find("p")
+        .should("contain.text", `Price: $${index}.00`);
+    });
+  });
+
+  it("removes product from cart when remove button is clicked", () => {
+    cy.get(".product-container")
+      .first()
+      .within(() => {
+        cy.get(".remove-button").click();
+      });
+    cy.get(".product-container").should("have.length"); 
+  });
+});
